d5b94e6bdb39ef4db6aceffa7420abb35a7b1bc4
IlyaGrutsyn <119618029+IlyaGrutsyn@users.noreply.github.com>
2024-02-06 07:57:33 +0600

    add dataset

---
+++ image_saver.py
@@ -0,0 +1,7 @@
+import 
+
+
+
+<div class="AdvancedCaptcha-ImageWrapper" data-testid="silhouette-container"><img src="https://smartcaptcha.yandexcloud.net/captchaimg?aHR0cHM6Ly9pbWcuc21hcnRjYXB0Y2hhLnlhbmRleGNsb3VkLm5ldC9pbWFnZT9rZXk9MDBaZUpNOGFMS0hxQlJLeGFycGl4YmNPV2cyaEZHa3omZGF0YT1pbWcmc2VydmljZT1vdGhlcg,,_4/1706950874/80c7d0bb36f36a4913d5cf36a59102dd_b15a15ec225c74aa467eb0ba1b377f53" alt="Задание с картинкой"></div>
+
+
